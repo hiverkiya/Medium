1385f268e0695a7981774fbe908773b61c03c0fe
hiverkiya <hiverkiya@gmail.com>
2022-10-01 00:31:39 +0530

    Fixing major bugs

--- medium/pages/api/createComment.ts
+++ medium/pages/api/createComment.ts
@@ -7,7 +7,7 @@ const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     useCdn: process.env.NODE_ENV === 'production',
-    token: process.env.SANITY_API_TOKEN
+    token: process.env.SANITY_API_TOKEN,
 }
 
 const client = sanityClient(config)
@@ -26,7 +26,7 @@ export default async function createComment(
             name,
             email,
             comment
-        })
+        });
     }
     catch(err){
         return res.status(500).json({message:' Error submitting comment', err})
